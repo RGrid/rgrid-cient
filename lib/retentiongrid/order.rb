9c369872db93c0d5a8c693a2e84690d7145d1c75
Christoph Bünte <info@christophbuente.de>
2014-07-24 23:59:34 +0200

    Add save and destroy methods to order.

--- lib/retentiongrid/order.rb
+++ lib/retentiongrid/order.rb
@@ -1,7 +1,13 @@
 module Retentiongrid
   class Order
-    attr_accessor :order_id, :customer_id, :status, :total_price, :total_discounts,
-                  :currency, :canceled_shiped, :canceled_shop_fault, :order_created_at
+
+    ATTRIBUTES_NAMES = [  :order_id, :customer_id, :status, :total_price, :total_discounts,
+                          :currency, :canceled_shiped, :canceled_shop_fault, :order_created_at
+                       ]
+
+    ATTRIBUTES_NAMES.each do |attrib|
+      attr_accessor attrib
+    end
 
     def initialize(attribs={})
       attribs.each do |attrib, value|
@@ -9,5 +15,21 @@ module Retentiongrid
       end
       @order_created_at = Time.parse(order_created_at) unless order_created_at.nil?
     end
+
+    def attributes
+      ATTRIBUTES_NAMES.inject({}) do |attribs, attrib_name|
+        attribs[attrib_name] = self.send(attrib_name)
+        attribs
+      end
+    end
+
+    def save
+      res = Api.post("/orders/#{order_id}", attributes)
+    end
+
+    def destroy
+      res = Api.delete("/orders/#{order_id}")
+    end
+
   end
 end
